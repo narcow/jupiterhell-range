4af2d6c35dea4026da6cf3a3ad8d4c6126b2d7bc
narcow <>
2021-08-15 15:19:38 -0400

    License and initial version

---
+++ gui.py
@@ -0,0 +1,109 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, QComboBox, QLabel
+from PyQt5.QtGui import QIcon
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+
+from jh_range import JupiterHellRangeVisualizer
+
+class App(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.left = 10
+        self.top = 10
+        self.title = 'Jupiter Hell Range Visualizer'
+        self.width = 800
+        self.height = 600
+        self.jhv = JupiterHellRangeVisualizer()
+        self.canvas = None
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.canvas = PlotCanvas(self, width=8, height=5.5)
+        self.canvas.move(0,0)
+
+        lbl_usage = QLabel(self)
+        lbl_usage.setText('Minimum / Optimal / Maximum:')
+        lbl_usage.adjustSize()
+        lbl_usage.move(150,568)
+
+        # minimum range. default: 2
+        cb_range_min = QComboBox(self)
+        cb_range_min.setToolTip('Minimum Range')
+        cb_range_min.move(350,560)
+        for i in range(1, 5):
+            cb_range_min.addItem(f'{i}', i)
+        cb_range_min.setCurrentIndex(cb_range_min.findData(2))
+        cb_range_min.currentTextChanged.connect(self._cb_range_min_update)
+
+        # optimal range. default: 3
+        cb_range_opt = QComboBox(self)
+        cb_range_opt.setToolTip('Optimal Range')
+        cb_range_opt.move(450,560)
+        for i in range(2, 10):
+            cb_range_opt.addItem(f'{i}', i)
+        cb_range_opt.setCurrentIndex(cb_range_opt.findData(3))
+        cb_range_opt.currentTextChanged.connect(self._cb_range_opt_update)
+
+        # maximum range. default: 6
+        cb_range_max = QComboBox(self)
+        cb_range_max.setToolTip('Maximum Range')
+        cb_range_max.move(550,560)
+        for i in range(3, 16):
+            cb_range_max.addItem(f'{i}', i)
+        cb_range_max.setCurrentIndex(cb_range_max.findData(6))
+        cb_range_max.currentTextChanged.connect(self._cb_range_max_update)
+
+        self.show()
+
+    def _cb_range_min_update(self, value):
+        print(f'set minimum range to {value}')
+        try:
+            min_range = int(value)
+        except ValueError:
+            min_range = 0
+        self.jhv.gun.update(min_range=min_range)
+        self.jhv.visualize(show=False, ax=self.canvas.subplot_ax)
+        self.canvas.draw()
+
+    def _cb_range_opt_update(self, value):
+        print(f'set optimal range to {value}')
+        self.jhv.gun.update(optimal_range=int(value))
+        self.jhv.visualize(show=False, ax=self.canvas.subplot_ax)
+        self.canvas.draw()
+
+    def _cb_range_max_update(self, value):
+        print(f'set maximum range to {value}')
+        self.jhv.gun.update(max_range=int(value))
+        self.jhv.visualize(show=False, ax=self.canvas.subplot_ax)
+        self.canvas.draw()
+
+class PlotCanvas(FigureCanvas):
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                QSizePolicy.Expanding,
+                QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+        
+        self.subplot_ax = self.figure.add_subplot(111)
+        parent.jhv.visualize(show=False, ax=self.subplot_ax)
+        self.draw()
+
+    def redraw(self):
+        self.draw()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
---
+++ jh_range.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class JupiterHellGun():
+    def __init__(self, minimum_range=0, optimal_range=3, maximum_range=6):
+        self.min = 2
+        self.optimal = 3
+        self.max = 6
+
+    def update(self, *, min_range=None, optimal_range=None, max_range=None):
+        self.min = min_range or self.min
+        self.optimal = optimal_range or self.optimal
+        self.max = max_range or self.max
+
+    def __str__(self):
+        if self.min > 0:
+            return f'{self.min}/{self.optimal}/{self.max}'
+        return f'{self.optimal}/{self.max}'
+
+class JupiterHellRangeVisualizer():
+    def __init__(self, area=7 ):
+        distance_matrix = np.zeros( [area+1,area+1] )
+        for x in range(area+1):
+            for y in range(area+1):
+                distance_matrix[y, x] = ( (x**2) + (y**2) ) ** 0.5
+        self._distance_matrix = distance_matrix
+
+        self.gun = JupiterHellGun()
+
+    def _to_hit(self, distance):
+        if distance < self.gun.min:
+            return distance / self.gun.min
+        elif distance <= self.gun.optimal:
+            return 1.0
+        else: 
+            return max( 0.0, 1.0 - ( ( distance - self.gun.optimal ) / ( 1 + self.gun.max - self.gun.optimal ) ) )
+
+    @property
+    def distance_matrix(self):
+        return self._distance_matrix
+
+    @property
+    def to_hit(self):
+        return np.vectorize(self._to_hit)(self.distance_matrix)
+
+    def visualize(self, show=True, out_filename=None, ax=None):
+        ax.clear()
+        sns.set_theme()
+        heatmap = sns.heatmap(self.to_hit, annot=True, cbar=False, ax=ax)
+        heatmap.invert_yaxis()
+        if out_filename is not None:
+            heatmap.set_title(f'Accuracy for {self.gun}')
+            heatmap.get_figure().savefig(out_filename)
+            print(f'{out_filename} saved.')
+        elif show:
+            heatmap.set_title(f'Accuracy for {self.gun}')
+            plt.show()
+
+
+if __name__ == '__main__':
+    v = JupiterHellRangeVisualizer()
+    try:
+        minr = int( input('Minimum Range? ' ) or 0 )
+        optr = int( input('Optimum Range? ' ) )
+        maxr = int( input('Maximum Range? ' ) )
+        v.gun.update( minimum_range=minr, optimal_range=optr, maximum_range=maxr )
+
+        out_filename = input('Enter a filename to save to file, or press enter to display: ') or None
+        v.visualize(out_filename=out_filename)
+    except ValueError:
+        raise
